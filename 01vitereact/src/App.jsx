01851e7b4c656b937ee75cdb9240b10ac247896a
Kritikasahota <sahotakritika029@gmail.com>
2025-09-01 21:24:36 +0530

    initial commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-
+import Chai from "./chai"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>hello world</h1>
+    <>
+      <Chai/>
+      <h1>hello world</h1>
+      <p>test para</p>
+    </>
+  
   )
 }
 
